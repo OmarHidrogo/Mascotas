815d7d14318772a7585aa2d862bde8fb8d2ce1d1
Omar Hidrogo <omar.083090@gmail.com>
2017-07-10 12:57:05 -0500

    apenas se esta creando el MascotaAdaptador

---
+++ app/src/main/java/com/omar_hidrogo_local/mascotas/MascotaAdaptador.java
@@ -0,0 +1,25 @@
+package com.omar_hidrogo_local.mascotas;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by tmhidrooma on 10/07/2017.
+ */
+
+public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder> {
+    @Override
+    public MascotaAdaptador.MascotaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MascotaAdaptador.MascotaViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
