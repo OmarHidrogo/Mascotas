9449be56ff9a255c072a4814081767e1c2d8e256
Omar Hidrogo <omar.083090@gmail.com>
2017-07-12 17:25:23 -0500

    Cardview completo, realizar el boton de regresar, top 5 y agregar mas mascotas

--- app/src/main/java/com/omar_hidrogo_local/mascotas/MainActivity.java
+++ app/src/main/java/com/omar_hidrogo_local/mascotas/MainActivity.java
@@ -2,12 +2,64 @@ package com.omar_hidrogo_local.mascotas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+//importar el id de RECYCLERVIEW asignado en activity_main
+import static com.omar_hidrogo_local.mascotas.R.id.rvMascotas;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Lista de Mascotas
+    ArrayList<Mascota> mascotas;
+
+    //variable de lista Mascotas
+    private RecyclerView listaMascotas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //enlazar la variable de tipo RecyclerView al id del layout
+        listaMascotas = (RecyclerView) findViewById(rvMascotas);
+
+        //declarar el administrador del recyclerview
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        //la lista de mascotas ligarla al LinearLayoutManager
+        listaMascotas.setLayoutManager(llm);
+        inicializarListaMascotas();
+        inicializaAdaptador();
+    }
+
+
+
+
+
+    //inicializar el adaptador
+    public MascotaAdaptador adaptador;
+    private  void inicializaAdaptador(){
+        adaptador = new MascotaAdaptador(mascotas, this);
+        listaMascotas.setAdapter(adaptador);
+    }
+
+
+    // inicializar  la lista de Mascotas mediante el constructor de Mascota
+    public  void  inicializarListaMascotas(){
+
+        mascotas = new ArrayList<Mascota>();
+
+        //Se llena el arreglo por medio del constructor creado en la clase Mascota
+        mascotas.add(new Mascota(R.drawable.p2, "SHIH TZU"));
+        mascotas.add(new Mascota(R.drawable.p6, "CHIHUAHUEÑO"));
+        mascotas.add(new Mascota(R.drawable.p9, "PUG"));
+        mascotas.add(new Mascota(R.drawable.p8, "BOXER"));
+        mascotas.add(new Mascota(R.drawable.p7, "LABRADOR RETRIEVER"));
+
     }
 }
--- app/src/main/java/com/omar_hidrogo_local/mascotas/MascotaAdaptador.java
+++ app/src/main/java/com/omar_hidrogo_local/mascotas/MascotaAdaptador.java
@@ -1,25 +1,81 @@
 package com.omar_hidrogo_local.mascotas;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
- * Created by tmhidrooma on 10/07/2017.
+ * Created by tmhidrooma on 12/07/2017.
  */
 
 public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder> {
-    @Override
-    public MascotaAdaptador.MascotaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+    ArrayList<Mascota> mascotas;
+    Activity activity;
+
+    //
+    public MascotaAdaptador(ArrayList<Mascota> mascotas, Activity activity){
+        this.mascotas=mascotas;
+        this.activity=activity;
+
     }
 
+
+
+    //inflar el layout y pasa a view hlder para que el obtenga los datos
     @Override
-    public void onBindViewHolder(MascotaAdaptador.MascotaViewHolder holder, int position) {
+    public MascotaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        //Ligar el Layout cardview_mascotas al Adaptador
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_mascota, parent, false);
+
+        //Se envia el View al constructor  MascotasViewHolder
+        return new MascotaViewHolder(v);
+    }
+
+    //Se Asocia cada elemento de la lista con cada vista
+    @Override
+    public void onBindViewHolder(MascotaViewHolder mascotaViewHolder, int position) {
+    final Mascota mascota = mascotas.get(position);
+
+        mascotaViewHolder.imgFoto.setImageResource(mascota.getFoto());
+        mascotaViewHolder.tvnombremascota.setText(mascota.getNombre());
+
+        mascotaViewHolder.imgFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, likes_mascotas.class);
+                activity.startActivity(intent);
+            }
+        });
 
     }
 
+    //Se declara el tamano de la lista
     @Override
     public int getItemCount() {
-        return 0;
+        return mascotas.size();
+    }
+
+    public class MascotaViewHolder extends RecyclerView.ViewHolder {
+
+        //Declarar las variables de los views en este caso las imagenes de las mascotas y el nombre
+        private ImageView imgFoto;
+        private TextView tvnombremascota;
+
+
+        //Constructor heredado de RecyclerView.ViewHolder
+        public MascotaViewHolder(View itemView) {
+            super(itemView);
+            imgFoto         = (ImageView) itemView.findViewById(R.id.imgFoto);
+            tvnombremascota = (TextView) itemView.findViewById(R.id.tvNombreMascota);
+        }
     }
 }
